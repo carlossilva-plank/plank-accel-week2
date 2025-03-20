6ee9b4eaf6cea410e530374f424cb05f5656513c
Carlos Magalhães Silva <carlos.silva@joinplank.com>
2025-03-20 16:56:31 -0300

    simple long term semantic memory storage tool

--- apps/agents/src/day4/memory/long-term/semanticsearch.ts
+++ apps/agents/src/day4/memory/long-term/semanticsearch.ts
@@ -1,8 +1,26 @@
 import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
-import { InMemoryStore, StateGraph } from "@langchain/langgraph";
+import {
+  InMemoryStore,
+  MessagesAnnotation,
+  StateGraph,
+} from "@langchain/langgraph";
 import { StateAnnotation } from "src/retrieval-agent/state.js";
 // import "@tensorflow/tfjs-backend-cpu";
 // import { TensorFlowEmbeddings } from "@langchain/community/embeddings/tensorflow";
+
+import { tool } from "@langchain/core/tools";
+import { LangGraphRunnableConfig } from "@langchain/langgraph";
+
+import { z } from "zod";
+import { v4 as uuidv4 } from "uuid";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { isToolMessage, ToolMessage } from "@langchain/core/messages";
+import { AIMessage } from "@langchain/core/messages";
+import { HumanMessage } from "@langchain/core/messages";
+import { isAIMessage } from "@langchain/core/messages";
+import { BaseMessage, isHumanMessage } from "@langchain/core/messages";
+import { SystemMessage } from "@langchain/core/messages";
+import { isSystemMessage } from "@langchain/core/messages";
 
 // const embeddings = new TensorFlowEmbeddings({});
 
@@ -49,3 +67,122 @@ for (const memory of memories) {
 }
 
 export const graph = new StateGraph(StateAnnotation).compile();
+
+const upsertMemoryTool = tool(
+  async ({ content }, config: LangGraphRunnableConfig): Promise<string> => {
+    const store = config.store as InMemoryStore;
+    if (!store) {
+      throw new Error("No store provided to tool.");
+    }
+    await store.put(
+      ["user_123", "memories"],
+      uuidv4(), // give each memory its own unique ID
+      { text: content }
+    );
+    return "Stored memory.";
+  },
+  {
+    name: "upsert_memory",
+    schema: z.object({
+      content: z.string().describe("The content of the memory to store."),
+    }),
+    description: "Upsert long-term memories.",
+  }
+);
+
+const addMemories = async (
+  state: typeof MessagesAnnotation.State,
+  config: LangGraphRunnableConfig
+) => {
+  const store = config.store as InMemoryStore;
+
+  if (!store) {
+    throw new Error("No store provided to state modifier.");
+  }
+
+  // Search based on user's last message
+  const items = await store.search(["user_123", "memories"], {
+    // Assume it's not a complex message
+    query: state.messages[state.messages.length - 1].content as string,
+    limit: 4,
+  });
+
+  const memories = items.length
+    ? `## Memories of user\n${items
+        .map((item) => `${item.value.text} (similarity: ${item.score})`)
+        .join("\n")}`
+    : "";
+
+  // Add retrieved memories to system message
+  return [
+    { role: "system", content: `You are a helpful assistant.\n${memories}` },
+    ...state.messages,
+  ];
+};
+
+const agent = createReactAgent({
+  llm: new ChatOpenAI({ model: "gpt-4o-mini" }),
+  tools: [upsertMemoryTool],
+  prompt: addMemories,
+  store: store,
+});
+
+function printMessages(messages: BaseMessage[]) {
+  for (const message of messages) {
+    if (isSystemMessage(message)) {
+      const systemMessage = message as SystemMessage;
+      console.log(`System: ${systemMessage.content}`);
+    } else if (isHumanMessage(message)) {
+      const humanMessage = message as HumanMessage;
+      console.log(`User: ${humanMessage.content}`);
+    } else if (isAIMessage(message)) {
+      const aiMessage = message as AIMessage;
+      if (aiMessage.content) {
+        console.log(`Assistant: ${aiMessage.content}`);
+      }
+      if (aiMessage.tool_calls) {
+        for (const toolCall of aiMessage.tool_calls) {
+          console.log(`\t${toolCall.name}(${JSON.stringify(toolCall.args)})`);
+        }
+      }
+    } else if (isToolMessage(message)) {
+      const toolMessage = message as ToolMessage;
+      console.log(
+        `\t\t${toolMessage.name} -> ${JSON.stringify(toolMessage.content)}`
+      );
+    }
+  }
+}
+
+let result = await agent.invoke({
+  messages: [
+    {
+      role: "user",
+      content: "I'm hungry. What should I eat?",
+    },
+  ],
+});
+
+printMessages(result.messages);
+
+result = await agent.invoke({
+  messages: [
+    {
+      role: "user",
+      content: "Please remember that every Thursday is trash day.",
+    },
+  ],
+});
+
+printMessages(result.messages);
+
+result = await agent.invoke({
+  messages: [
+    {
+      role: "user",
+      content: "When am I supposed to take out the garbage?",
+    },
+  ],
+});
+
+printMessages(result.messages);
